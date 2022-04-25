b499e8f3ae3d9a48d15b5c153503ed38be025bc3
Raul Santos <raulsntos@gmail.com>
2022-04-25 19:12:42 +0200

    Update to 7.71.2

--- default.nix
+++ default.nix
@@ -2,22 +2,23 @@
 
 buildGoModule rec {
   pname = "oh-my-posh";
-  version = "7.70.0";
-
-  vendorSha256 = "sha256-AzfNq6t+Zk4eI0v6lWTzpcEmHPxF/3Lrk9g/OgOoTmI=";
+  version = "7.71.2";
 
   src = fetchFromGitHub {
     owner = "JanDeDobbeleer";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-4EK9Qw3Zj6yTHJJgnFbjpyp0YZG1uqHvOUi91OGBkAo=";
-  } + "/src";
+    sha256 = "sha256-T2xEsbeggXEvt802nWIb+lwy2odAmtWgdvPcEa6BPp0=";
+  };
+  modRoot = "./src";
+
+  vendorSha256 = "sha256-AzfNq6t+Zk4eI0v6lWTzpcEmHPxF/3Lrk9g/OgOoTmI=";
 
   ldflags = [ "-s" "-w" "-X" "main.Version=${version}" "-extldflags" "\"-static\"" ];
 
   meta = {
-    description = "A prompt theme engine for any shell.";
-    homepage = "https://github.com/jandedobbeleer/oh-my-posh";
+    description = "Cross platform, highly customizable and extensible prompt theme engine for any shell.";
+    homepage = "https://ohmyposh.dev/";
     license = lib.licenses.mit;
 
     platforms = lib.platforms.unix;
